a0a9e684ba386a12cf3c42dd556a9d0b5b944f79
Furnac <shmarglefoil@gmail.com>
2017-12-09 16:18:00 -0500

    woah

--- src/automaton/GameOfLifeTester.java
+++ src/automaton/GameOfLifeTester.java
@@ -6,15 +6,17 @@ public class GameOfLifeTester {
   public static void main(String[] args) {
     System.out.println("Hello!");
     Scanner scan = new Scanner(System.in);
-    Automaton2D gol = new Automaton2D(Automaton2D.MOORE, 20);
+    Automaton2D gol = new Automaton2D(Automaton2D.MOORE, 50);
     gol.genRuleSet(0);
     //gol.setRules(0,0,"=",0);
+    gol.init();
+    
     gol.setRules(1, 2, "<", 0);
     gol.setRules(1, 2, "=", 1);
     gol.setRules(1,3,"=",1);
     gol.setRules(1,3,">",0);
     gol.setRules(0,3,"=",1);
-    //gol.init();
+    /*
     gol.set(1, 5, 5);
     gol.set(1, 5, 6);
     gol.set(1, 5, 7);
@@ -28,6 +30,7 @@ public class GameOfLifeTester {
     gol.set(1, 4, 9);
     gol.set(1, 6, 4);
     gol.set(1, 6, 9);
+    */
     System.out.println(gol.toString().replace("0", "  ").replace("1", "[]"));
     while(!scan.nextLine().equals("STOP")) {
       gol.step();

1c3048a170250ed05cdb00a7519c74c854e48c3e
Furnac <shmarglefoil@gmail.com>
2017-12-09 17:33:02 -0500

    almost scenes

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,4 +1,334 @@
-package GUI;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 public class Controller {
+  
+  @FXML
+  private RadioButton ca1d_010rb;
+  
+  @FXML
+  private Slider ca2d_heightSlider;
+  
+  @FXML
+  private RadioButton ca1d_011rb;
+  
+  @FXML
+  private RadioButton ca2d_b2RadioButton;
+  
+  @FXML
+  private TextField ca1d_ruleTextField;
+  
+  @FXML
+  private RadioButton ca2d_s1RadioButton;
+  
+  @FXML
+  private RadioButton ca2d_s7RadioButton;
+  
+  @FXML
+  private RadioButton ca1d_100rb;
+  
+  @FXML
+  private RadioButton ca2d_s3RadioButton;
+  
+  @FXML
+  private RadioButton ca2d_b3RadioButton;
+  
+  @FXML
+  private RadioButton ca1d_101rb;
+  
+  @FXML
+  private Canvas ca1d_canvas;
+  
+  @FXML
+  private RadioButton ca2d_s6RadioButton;
+  
+  @FXML
+  private RadioButton ca2d_b6RadioButton;
+  
+  @FXML
+  private RadioButton ca2d_b4RadioButton;
+  
+  @FXML
+  private Slider ca1d_heightSlider;
+  
+  @FXML
+  private RadioButton ca2d_s2RadioButton;
+  
+  @FXML
+  private Slider ca1d_widthSlider;
+  
+  @FXML
+  private Canvas ca2d_canvas;
+  
+  @FXML
+  private RadioButton ca1d_000rb;
+  
+  @FXML
+  private RadioButton ca1d_001rb;
+  
+  @FXML
+  private RadioButton ca2d_b7RadioButton;
+  
+  @FXML
+  private RadioButton ca2d_s5RadioButton;
+  
+  @FXML
+  private Slider ca2d_widthSlider;
+  
+  @FXML
+  private RadioButton ca1d_111rb;
+  
+  @FXML
+  private RadioButton ca1d_110rb;
+  
+  @FXML
+  private RadioButton ca2d_b5RadioButton;
+  
+  @FXML
+  private RadioButton ca2d_b0RadioButton;
+  
+  @FXML
+  private ComboBox<?> ca2d_neighborhoodComboBox;
+  
+  @FXML
+  private RadioButton ca2d_b1RadioButton;
+  
+  @FXML
+  private Slider ca2d_speedSlider;
+  
+  @FXML
+  private RadioButton ca2d_s0RadioButton;
+  
+  @FXML
+  private Slider ca1d_speedSlider;
+  
+  @FXML
+  private RadioButton ca2d_b8RadioButton;
+  
+  @FXML
+  private RadioButton ca2d_s4RadioButton;
+  
+  @FXML
+  private RadioButton ca2d_s8RadioButton;
+  
+  @FXML
+  void closeApplication(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_runToggle(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_step(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_initialize(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_clear(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setSpeed(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setWidth(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setHeight(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesRad(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesRad(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesRad(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesRad(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesRad(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesRad(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesRad(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesRad(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca1d_setRulesText(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_runToggle(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_step(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d__initialize(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_clear(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setSpeed(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setWIdth(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setHeight(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setRulesMultiple(ActionEvent event) {
+  
+  }
+  
+  @FXML
+  void ca2d_setNeighborhood(ActionEvent event) {
+  
+  }
+  
 }
